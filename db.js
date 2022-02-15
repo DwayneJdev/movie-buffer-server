b2dcbaa93e9b740a81325920298185b3d08c41b4
DwayneJdev <dwaynej14@gmail.com>
2022-02-15 18:35:45 -0500

    ssl

--- db.js
+++ db.js
@@ -1,7 +1,21 @@
 const {Sequelize} = require('sequelize');
 
-const seqeulize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: process.env.DATABASE_DIALECT
-})
+const seqeulize = new Sequelize(process.env.DATABASE_URL, 
+    
+    process.env.HOST === 'local' ? {
+        dialect: process.env.DATABASE_DIALECT,
+    
+      }
+        :
+        {
+            dialect: process.env.DATABASE_DIALECT,
+          dialectOptions: {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false
+            }
+          }
+        }
+)
 
 module.exports = seqeulize;
