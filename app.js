c05898155d99fbfd2d47e553c5f16251caed9afe
DwayneJdev <dwaynej14@gmail.com>
2022-02-21 19:26:06 -0500

    took out suggestion

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 
-const cors = require('cors')
+// const cors = require('cors')
 const express = require('express');
 const app = express();
 
-let whitelist = ['http://localhost:3001', 'https://dkj-movie-buffer-client.herokuapp.com', 'http://localhost:3000' ]
-app.use(cors({origin: whitelist, credentials: true}));
+// let whitelist = ['http://localhost:3001', 'https://dkj-movie-buffer-client.herokuapp.com', 'http://localhost:3000' ]
+// app.use(cors({origin: whitelist, credentials: true}));
 
 const dbConnection = require('./db');
 const controllers  = require('./controllers');
